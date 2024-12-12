6a0cd3a2192b04cb70b18881a9f3fc4219af1b44
tamaza <tamaz.benidze16@gmail.com>
2024-12-12 07:27:06 +0400

    განახლება 1.0

---
+++ README.md
@@ -0,0 +1,5 @@
+## SonsHoodie
+- Jacket - 79 
+## SonsProspect
+- Bags and parachute - 58
+- Bags and parachute - 59
---
+++ SonsProspect/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'tazo'
+
+files {
+    'mp_m_freemode_01_mp_m_january2016^hand_006_u.ydd',
+    'mp_m_freemode_01_mp_m_january2016^hand_diff_006_a_uni.ytd'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/mp_m_freemode_01_mp_m_january2016^hand_006_u.ydd'
